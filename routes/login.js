3a0c405b99c2e322767c88e73246dfa6c86dd8d6
Ilya <iachernikov1@gmail.com>
2017-10-27 21:02:58 +0300

    v_2

--- routes/login.js
+++ routes/login.js
@@ -1,13 +1,24 @@
 const passport = require('koa-passport');
 const compose = require('koa-compose');
+var session=require('../libs/mongoose');
 let User = require('../models/user');
 const mongoose=require('mongoose');
 exports.post = async (ctx, next) => {
     //await passport.authenticate('local');
     var a =await auth(ctx);
     if (ctx.state.user) {
-        ctx.body={id:ctx.sessionId};
-        //ctx.redirect('/');
+        var ses={
+            sid:`koa:sess:${ctx.sessionId}`,
+            blob:'{"cookie":{"httpOnly":true,"path":"/","overwrite":true,"signed":false,"maxAge":14400000}}'
+        };
+        var curSes=new session.models.Session(ses);
+        try {
+            await curSes.save();
+        }
+        catch (e){
+
+        }
+        ctx.redirect('/');
     } else {
       ctx.status = 401;
       ctx.body = { error: 'err'};
